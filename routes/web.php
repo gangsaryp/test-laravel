7cf5cfe423121ae28066c0fd18d1d4656ea3fdd1
gangsaryp <gangsaryoga1996@gmail.com>
2021-06-08 15:25:42 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
 });
 
 Route::get('/categories','CategoryController@index');
